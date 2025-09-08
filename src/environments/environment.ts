c78b4263b65fc281f05c604d6cc540596ba21809
fernandoorellano <fm_orellano@outlook.com>
2025-09-08 17:00:50 -0300

    mejoras

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,22 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAGb1mzcndTeQYZNDIBi2DfsiZZykB7fUA",
-    authDomain: "ftth-firebase-160f5.firebaseapp.com",
-    projectId: "ftth-firebase-160f5",
-    storageBucket: "ftth-firebase-160f5.firebasestorage.app",
-    messagingSenderId: "260102642918",
-    appId: "1:260102642918:web:afd31cfdce214088b02f27",
-  },
-  auth :{
-    usuario: "admin",
-    contraseña: "admin7"
-  }
-  
-};
+export const environment = {};
 
 /*
  * For easier debugging in development mode, you can import the following file
