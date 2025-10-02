e080465c90228a127dd162e516f8e00d5d26421c
fernandoorellano <fm_orellano@outlook.com>
2025-10-02 16:11:46 -0300

    mejoras

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAGb1mzcndTeQYZNDIBi2DfsiZZykB7fUA",
+    authDomain: "ftth-firebase-160f5.firebaseapp.com",
+    projectId: "ftth-firebase-160f5",
+    storageBucket: "ftth-firebase-160f5.firebasestorage.app",
+    messagingSenderId: "260102642918",
+    appId: "1:260102642918:web:afd31cfdce214088b02f27"
+  },
+  auth :{
+    usuario: "admin",
+    contraseña: "admin7"
+  }
+  
+};
